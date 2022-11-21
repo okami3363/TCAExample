a17ef070fada290f8a358a3b8b26ab69ee97dad6
david.huang <david.huang@ihwic.com>
2022-11-21 17:54:49 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TCAExample
+
+網上教學的範例實作
+https://onevcat.com/2021/12/tca-1/
---
+++ TCAExample/TCAExample/TCAExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAExampleApp.swift
+//  TCAExample
+//
+//  Created by david.huang on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TCAExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
